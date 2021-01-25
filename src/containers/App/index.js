9e3c1c9c285023f18af25da696305bf02db69189
peter-keller <keller.f.peter@gmail.com>
2021-01-25 22:07:41 +0000

    feat: v1 basic structure and functionality

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,12 +1,15 @@
 import React from "react";
+
+import Header from "../../components/Header";
+import Profile from "../Profile";
+
 import "./App.css";
-import Header from "../components/Header/Header";
-import logo from "../logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <Header logo={logo} />
+      <Header />
+      <Profile />
     </div>
   );
 }
