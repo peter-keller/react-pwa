fe1f4fffb4534029d02afd41bc27688f066a0673
peter-keller <keller.f.peter@gmail.com>
2021-01-25 19:23:51 +0000

    feat: componentized structure, pwa setup

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Header from "../components/Header/Header";
+import logo from "../logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <Header logo={logo} />
+    </div>
+  );
+}
+
+export default App;
